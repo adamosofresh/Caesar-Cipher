cb5f299500e8c350f32da399ec5059ea32032aab
adamosofresh <131510443+adamosofresh@users.noreply.github.com>
2023-04-23 22:00:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caesar (message=text, shift=shift, direction=direction):
+    message = " "
+    for i in text:
+        index_letter = alphabet.index(i)
+        if direction == "encode":
+            position = index_letter + shift
+        else:
+            position = index_letter - shift
+        new_text = alphabet[position]
+        message += new_text
+    print(f"The decoded text is{message}")
+
+
+caesar(text, shift, direction)
